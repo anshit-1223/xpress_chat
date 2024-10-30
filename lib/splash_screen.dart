968a17c8394f04209717c91fd70f671590c7b4ce
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-10-30 21:38:58 +0530

    Changed to Stateful

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:nativewrappers/_internal/vm/lib/async_patch.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:xpress_chat/login_page.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer();
+  }
 
   @override
   Widget build(BuildContext context) {
