1115fc929f9aa82a9204cdb8c6d0b839d6190da1
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-10-31 22:17:36 +0530

    Add homepage route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:xpress_chat/home_page.dart';
 import 'package:xpress_chat/login_page.dart';
 import 'package:xpress_chat/register_page.dart';
 import 'package:xpress_chat/splash_screen.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         "/": (context) => SplashScreen(),
         "/login": (context) => LoginPage(),
         "/register": (context) => RegisterPage(),
+        "/home": (context) => HomePage(),
       },
     );
   }
