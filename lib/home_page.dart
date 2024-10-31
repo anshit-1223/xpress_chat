8dc2d65683be9b86556ed9767299d563145ccd60
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-10-31 22:14:58 +0530

    Added Homepage

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Title(color: Colors.white, child: Text("Xpress Chat")),
+      ),
+      body: Center(
+        child: Text("HomePage"),
+      ),
+    );
+  }
+}
